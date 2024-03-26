35530d8669a7c8a5aa9b585453f7cc8c958cd269
Marie Metay <118732275+MarieM512@users.noreply.github.com>
2024-03-26 19:55:46 +0100

    Feat/authentication token (#1)
    * Add configuration spring security
    
    * Add services for JWT
    
    * Add Spring Web dependencie
    
    * Add AuthController with fake endpoints for tests

--- src/main/java/com/mariemetay/ChatopapiApplication.java
+++ src/main/java/com/mariemetay/ChatopapiApplication.java
@@ -1,4 +1,4 @@
-package com.mariemetay.chatopapi;
+package com.mariemetay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
