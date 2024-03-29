a329142c688a679d3ce3144f3156f45873160d01
Marie Metay <mariemetay.pro@gmail.com>
2024-03-29 19:16:10 +0100

    Link swagger with user by id

--- src/main/java/com/mariemetay/controller/UserController.java
+++ src/main/java/com/mariemetay/controller/UserController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mariemetay.model.dto.UserDTO;
 import com.mariemetay.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -25,7 +31,15 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Display specific user by its id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieve", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = UserDTO.class))
+        }),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+    })
     @GetMapping("/{id}")
+    @SecurityRequirement(name = "Authorization")
     public ResponseEntity<UserDTO> getUserById(HttpServletRequest request, @PathVariable("id") Long id) {
         String bearerToken = request.getHeader("Authorization");
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
