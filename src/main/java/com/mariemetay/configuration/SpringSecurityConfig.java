8ead7a88aa73bd8f462004a53ef29a9b6c680c25
Marie Metay <mariemetay.pro@gmail.com>
2024-04-03 14:41:23 +0200

    Disable default auth

--- src/main/java/com/mariemetay/configuration/SpringSecurityConfig.java
+++ src/main/java/com/mariemetay/configuration/SpringSecurityConfig.java
@@ -34,7 +34,6 @@ public class SpringSecurityConfig {
                 .requestMatchers("/backend/img/**").permitAll()
                 .anyRequest().authenticated()
             )
-            .httpBasic(Customizer.withDefaults())
             .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
             .build();
     }
