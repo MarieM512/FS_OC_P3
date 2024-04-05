e270784a8cbbfb1ccd3fbd7d4b7a98dd9b2f8d0f
Marie Metay <118732275+MarieM512@users.noreply.github.com>
2024-04-05 18:01:52 +0200

    Refactor/file storage (#9)
    * Add error if file is not an image
    
    * Modify response for read image
    
    * Add swagger for file controller

--- src/main/java/com/mariemetay/controller/FileControlller.java
+++ src/main/java/com/mariemetay/controller/FileControlller.java
@@ -2,7 +2,11 @@ package com.mariemetay.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
@@ -15,12 +19,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
+@Tag(name = "File", description = "Permit to display image")
 @RestController
 public class FileControlller {
 
     private String fileStorageLocation = "src/main/resources/img";
 
+    @Operation(summary = "Display image")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful loading", content = @Content),
+        @ApiResponse(responseCode = "404", description = "File does not exist", content = @Content),
+        @ApiResponse(responseCode = "500", description = "Fail to load resource", content = @Content)
+    })
     @GetMapping("/backend/img/{fileName:.+}")
     public ResponseEntity<Resource> getImage(@PathVariable("fileName") String fileName) throws FileNotFoundException {
         
@@ -29,13 +39,13 @@ public class FileControlller {
 
         try {
             resource = new UrlResource(filePath.toUri());
+            if (!resource.exists()) {
+                return ResponseEntity.status(404).build();
+            } else {
+                return ResponseEntity.ok().body(resource);
+            }
         } catch (MalformedURLException e) {
-            throw new FileNotFoundException("File not found" + fileName);
+            return ResponseEntity.status(500).build();
         }
-        if (!resource.exists()) {
-            throw new FileNotFoundException("File not found" + fileName);
-        }
-        return ResponseEntity.ok().body(resource);
     }
-
 }
